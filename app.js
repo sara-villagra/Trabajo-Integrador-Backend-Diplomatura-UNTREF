a39a39369f5d06ef6cb515ddc4cc5823ef54acdc
sarahvill <130417177+sara-villagra@users.noreply.github.com>
2024-06-20 20:30:36 -0300

    agregué conexión a BD,archivo de esquema

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./src/database')
 process.loadEnvFile()
 const port = process.env.PORT ?? 3000
 const morgan = require('morgan')
-
+const Accesorio = require('./src/product')
+connectDB()
 //Middleware
 app.use(express.json())
 app.use(morgan('dev'))
@@ -12,6 +14,13 @@ app.use(morgan('dev'))
 app.get('/', (req, res) => {
  res.send('Bienvenido a la API de Accesorios de Computación !')
 })
+//obtener todos los accesorios
+
+app.get('/accesorios', async (req, res) => {
+ const accesorios = await Accesorio.find()
+ res.json(accesorios)
+})
+//obtener un accesorio
 
 //Inicializamos el servidor
 app.listen(port, () => {
---
+++ src/product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const accesoriosSchema = new mongoose.Schema({
+ //title: String,
+ codigo: Number,
+ nombre: String,
+ precio: Number,
+ categoria: String
+})
+const Accesorio = mongoose.model('accesorio', accesoriosSchema)
+module.exports = Accesorio
